8df6894e4098a4a85c1bc4c474259572b63296d5
guerinorodella <guerinorodella@gmail.com>
2024-12-07 14:28:09 -0300

    feat(api): added api functionalities

---
+++ src/main/kotlin/com/gear/dev/voucher/api/VoucherApiApplication.kt
@@ -0,0 +1,11 @@
+package com.gear.dev.voucher.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoucherApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoucherApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/gear/dev/voucher/api/VoucherApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gear.dev.voucher.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoucherApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
